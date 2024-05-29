26707a31ae36d41737c1547c1547d2f97205d549
Umar <77452832+UmarUMahmood@users.noreply.github.com>
2024-05-29 03:13:27 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script></script>
+
+<template>
+  <div class="column1">
+    <img class="map1" src="./assets/maps/samoa.jpg" alt="Map Image 1" />
+  </div>
+  <div class="column2">
+    <img class="map2" src="./assets/maps/ilios.jpg" alt="Map Image 1" />
+  </div>
+</template>
+
+<style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+
+.map1,
+.map2 {
+  height: 100vh;
+  width: 50%;
+  position: absolute;
+  object-fit: cover;
+}
+
+.map1 {
+  left: 0;
+}
+
+.map2 {
+  right: 0%;
+}
+</style>
