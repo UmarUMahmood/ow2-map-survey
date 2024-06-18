1b52218037e8350a5aec9812176a61f4a5cd2bff
Umar <77452832+UmarUMahmood@users.noreply.github.com>
2024-06-18 11:39:13 +0100

    run formatting

--- server/db.js
+++ server/db.js
@@ -1,55 +1,55 @@
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
 
-dotenv.config();
+dotenv.config()
 
-const url = process.env.MONGODB_URL;
-const dbName = "votes_db";
+const url = process.env.MONGODB_URL
+const dbName = "votes_db"
 let client
 let db
 
 export async function initialiseDb() {
-    if (!client) {
-        client = new MongoClient(url)
+  if (!client) {
+    client = new MongoClient(url)
 
-        try {
-            await client.connect()
-            db = client.db(dbName)
-            console.log("Connected successfully to MongoDB")
-        } catch (err) {
-            console.error("Error connecting to MongoDB: ", err)
-            throw err
-        }
+    try {
+      await client.connect()
+      db = client.db(dbName)
+      console.log("Connected successfully to MongoDB")
+    } catch (err) {
+      console.error("Error connecting to MongoDB: ", err)
+      throw err
     }
+  }
 }
 
 export async function getVotes() {
-    try {
-        console.log("db: ", db)
-        if (!db) await initialiseDb()
-        const collection = db.collection("votes");
-        const votes = await collection.find({}).toArray();
-        return votes;
-    } catch (err) {
-        console.error("Error retrieving votes: ", err);
-    }
+  try {
+    console.log("db: ", db)
+    if (!db) await initialiseDb()
+    const collection = db.collection("votes")
+    const votes = await collection.find({}).toArray()
+    return votes
+  } catch (err) {
+    console.error("Error retrieving votes: ", err)
+  }
 }
 
 export async function addVote(map1, map2, voted) {
-    try {
-        if (!db) await initialiseDb()
-        const collection = db.collection("votes");
-        const result = await collection.insertOne({ map1, map2, voted });
-        console.log(`New vote inserted with the following id: ${result.insertedId}`);
-    } catch (err) {
-        console.error("Error adding vote: ", err);
-    }
+  try {
+    if (!db) await initialiseDb()
+    const collection = db.collection("votes")
+    const result = await collection.insertOne({ map1, map2, voted })
+    console.log(`New vote inserted with the following id: ${result.insertedId}`)
+  } catch (err) {
+    console.error("Error adding vote: ", err)
+  }
 }
 
 process.on("SIGINT", async () => {
-    if (client) {
-        await client.close()
-        console.log("MongoDB connected closed")
-    }
-    process.exit(0);
+  if (client) {
+    await client.close()
+    console.log("MongoDB connected closed")
+  }
+  process.exit(0)
 })
