85e030a7f3599ecac498cc72e0ddf8224b096f75
Umar <77452832+UmarUMahmood@users.noreply.github.com>
2024-06-18 10:56:30 +0100

    improve connection handling for db

--- server/db.js
+++ server/db.js
@@ -5,11 +5,27 @@ dotenv.config({ path: "../.env"});
 
 const url = process.env.MONGODB_URL;
 const dbName = "votes_db";
-const client = new MongoClient(url);
+let client
+let db
+
+export async function initialiseDb() {
+    if (!client) {
+        client = new MongoClient(url)
+
+        try {
+            await client.connect()
+            db = client.db(dbName)
+            console.log("Connected successfully to MongoDB")
+        } catch (err) {
+            console.error("Error connecting to MongoDB: ", err)
+            throw err
+        }
+    }
+}
 
 export async function getVotes() {
     try {
-        const db = client.db(dbName);
+        if (!db) await initialiseDb()
         const collection = db.collection("votes");
         const votes = await collection.find({}).toArray();
         return votes;
@@ -20,7 +36,7 @@ export async function getVotes() {
 
 export async function addVote(map1, map2, voted) {
     try {
-        const db = client.db(dbName);
+        if (!db) await initialiseDb()
         const collection = db.collection("votes");
         const result = await collection.insertOne({ map1, map2, voted });
         console.log(`New vote inserted with the following id: ${result.insertedId}`);
@@ -28,3 +44,11 @@ export async function addVote(map1, map2, voted) {
         console.error("Error adding vote: ", err);
     }
 }
+
+process.on("SIGINT", async () => {
+    if (client) {
+        await client.close()
+        console.log("MongoDB connected closed")
+    }
+    process.exit(0);
+})
